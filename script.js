dc0c952cd72dc6805007bd4c4a5a3654f70f450c
unknown voices <unkv.sick@icloud.com>
2026-05-01 13:14:53 +0200

    prompt 1

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateQR() {
+  const text = document.getElementById("text").value;
+  const qrContainer = document.getElementById("qrcode");
+  qrContainer.innerHTML = "";
+
+  new QRCode(qrContainer, {
+    text: text,
+    width: 200,
+    height: 200
+  });
+}
